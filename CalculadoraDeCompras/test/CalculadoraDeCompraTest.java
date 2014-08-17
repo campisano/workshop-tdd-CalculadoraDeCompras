82ef6f7e44a4fd26876ba912effd568ae00e9f40
t1t0 <t1t0@macnix>
2014-08-17 12:46:13 -0300

    Test first - not compile

---
+++ CalculadoraDeCompras/test/CalculadoraDeCompraTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+// itens + frete - desconto
+public class CalculadoraDeCompraTest {
+
+	private CalculadoraDeCompra calc;
+	private IFrete frete;
+	private IDesconto desconto;
+	private List<Compras> compras;
+	double valor;
+
+	@Before
+	public void before() {
+		calc = new CalculadoraDeCompra(frete, desconto);
+		valor = 0;
+	}
+
+	@Test
+	public void valorDeveSumarOsItens() {
+		compras = new CompraBuilder().add(10).add(20).add(30).build();
+
+		valor = calc.calcula(compras);
+
+		Assert.assertEquals(60.0, valor, 0.001);
+	}
+}
