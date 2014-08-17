8d8b9c7c4a2346cb5a71f7a3bab2486d581db980
t1t0 <t1t0@macnix>
2014-08-17 14:35:30 -0300

    test build

--- CalculadoraDeCompras/src/CalculadoraDeCompra.java
+++ CalculadoraDeCompras/src/CalculadoraDeCompra.java
@@ -2,15 +2,16 @@ public class CalculadoraDeCompra {
 
 	private Compra compra;
 	private IFrete frete;
+	private IDesconto desconto;
 
-	public CalculadoraDeCompra(Compra compra, IFrete frete) {
+	public CalculadoraDeCompra(Compra compra, IFrete frete, IDesconto desconto) {
 		this.compra = compra;
 		this.frete = frete;
+		this.desconto = desconto;
 	}
 
 	public double calcula() {
-		// compra.valor + frete(compra) - desconto(conta);
-
-		return compra.getValor() + frete.getValor(compra);
+		return compra.getValor() + frete.getValor(compra)
+				- desconto.getValor(compra);
 	}
 }
---
+++ CalculadoraDeCompras/src/DescontoPorValor.java
@@ -0,0 +1,9 @@
+
+public class DescontoPorValor implements IDesconto {
+
+	public double getValor(Compra compra) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
